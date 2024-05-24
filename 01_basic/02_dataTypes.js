9845cd87b4c3a4a93334df43545ae4676d01ebf2
MdTohidReza <141931956+MdTohidReza@users.noreply.github.com>
2024-05-24 18:19:22 +0000

    02_dataTypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict";//treat all js code as newer vesrion 
+
+//alert(3+3) we are using nodejs, not browser
+//code readilibity must be high
+console.log(3+3);
+console.log( "Hello")
+let name = "Tohid"
+let age = 21
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint is used when our number is very big like trading etc
+// string=>"" written in double code or in single code
+// boolean => either true or false
+//null=> standalone value
+//undefined =>
+// symbol=> to find unique components
+// the type of undefined is
+// null is an object
+
+// object
+
+console.log(typeof "Tohid");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
