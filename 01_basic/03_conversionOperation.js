1699f55eb78b9f8d91a92bad59743370f16f4203
MdTohidReza <141931956+MdTohidReza@users.noreply.github.com>
2024-05-31 18:14:04 +0000

    modified

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -24,7 +24,43 @@
 // when we write 1 in boolean then it convert into true in boolean and 0,empty for false
 // if we have some value in string then it convert into true in boolean
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log( typeof stringNumber)
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log( typeof stringNumber)
+
+
+
+// ************************Operations***************************
+
+let value  = 3
+let negValue = -value
+//console.log(negValue );
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 ="hello"
+let str2 = " tohid"
+let str3 = str1 + str2;
+console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+console.log(+true)
+console.log(+"")
+
+
+let num1, num2,num3
+num1=num2=num3=2+2// not write that way it is write but not good sense for writting code in this way
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter)
