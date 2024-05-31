7292e2197c37c6702cbaa69b50aab3f12d9e3207
MdTohidReza <141931956+MdTohidReza@users.noreply.github.com>
2024-05-31 17:42:23 +0000

    Conversion of dataTypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,30 @@
+//let score = "33"
+//let Score = "32abc"
+//let mark = null;
+//let score = undefined;
+// let score = true;
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let valueInNumber = Number(score);
+//let valueOnNumber = Number(Score);
+//console.log(typeof valueInNumber);
+//console.log( valueInNumber);
+
+// "33 " => 33(convert simply into number)
+// "33abc"=> NaN(not a number)
+// true => 1; false=>0
+
+//let isLoggedIn = "tohid"
+
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn )
+
+// when we write 1 in boolean then it convert into true in boolean and 0,empty for false
+// if we have some value in string then it convert into true in boolean
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log( typeof stringNumber)
