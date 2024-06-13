192c18cf40219009dc67a2e6b1ac2782f8a929ca
MdTohidReza <141931956+MdTohidReza@users.noreply.github.com>
2024-06-13 16:54:17 +0000

    comparision

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,19 @@
+//  comparision operator:
+
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined==0);
+
+// strict check means to check by triple ===.it check  our value strictly and also check datatypes
+console.log("2"===2); 
