b018dc291690d8af4339ece9281f741d121bde16
MdTohidReza <141931956+MdTohidReza@users.noreply.github.com>
2024-09-11 19:23:50 +0000

    nums and math

---
+++ 01_basic/06_nums_and_math.js
@@ -0,0 +1,33 @@
+// const score = 400;
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(4));
+
+// const hunrdeds = 1000000
+// console.log(hunrdeds.toLocaleString('en-IN'));
+// ++++++++++++++++++Maths++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.65));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+// console.log(Math.sqrt(4));
+// console.log(Math.min(3,5,7,1,3));
+// console.log(Math.max(2,5,7,8,9,1,2));
+// values of random lies between 0 and 1
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
