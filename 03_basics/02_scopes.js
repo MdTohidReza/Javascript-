88c0fd5312f35fc1dc3c2cd4c0b7bd3eafd0ba9e
MdTohidReza <141931956+MdTohidReza@users.noreply.github.com>
2024-09-20 16:22:13 +0000

    scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,8 +5,55 @@ if(true)
 let a = 10
 const b = 20
 //var c = 30
-console.log("INNER : ",a );
+//console.log("INNER : ",a );
 }
- console.log(a);
+//console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one()
+{
+    const username ="Tohid"
+
+    function two()
+    {
+        const website ="Youtube"
+        //console.log(username);
+        
+    }
+    //console.log(website);
+    two()
+}
+//one()
+
+
+if(true)
+{
+    const username ="Reza"
+    if(username==="Reza")
+    {
+        const website=" telegram"
+        //console.log(username+website);
+        
+    }
+    //console.log(website);
+    
+}
+//console.log(username);
+
+
+//==================== imteresting========================
+console.log(addone(5))
+function addone(num)
+{
+    return num+1
+}
+
+
+// error comes in this function because function is stored in variabke before decleration
+//console.log(addTwo(5))
+const addTwo = function(num)
+{
+    return num+2
+}
