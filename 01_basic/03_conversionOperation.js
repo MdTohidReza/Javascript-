3185c94844298e1162f29ec73bd03eec2e4c7bf3
MdTohidReza <141931956+MdTohidReza@users.noreply.github.com>
2024-11-18 15:14:15 +0000

    07_projects

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,11 +1,11 @@
-//let score = "33"
-//let Score = "32abc"
-//let mark = null;
-//let score = undefined;
+let score = "33"
+// let Score = "32abc"
+// let mark = null;
+// let score = undefined;
 // let score = true;
 
-//console.log(typeof score);
-//console.log(typeof(score));
+console.log(typeof score);
+console.log(typeof(score));
 
 //let valueInNumber = Number(score);
 //let valueOnNumber = Number(Score);
@@ -33,8 +33,8 @@
 
 // ************************Operations***************************
 
-let value  = 3
-let negValue = -value
+// let value  = 3
+// let negValue = -value
 //console.log(negValue );
 
 // console.log(2+2);
@@ -44,23 +44,23 @@ let negValue = -value
 // console.log(2/2);
 // console.log(2%2);
 
-let str1 ="hello"
-let str2 = " tohid"
-let str3 = str1 + str2;
-console.log(str3);
+// let str1 ="hello"
+// let str2 = " tohid"
+// let str3 = str1 + str2;
+// console.log(str3);
 
 // console.log("1"+2);
 // console.log(1+"2");
 // console.log("1"+2+2)
 // console.log(1+2+"2")
 
-console.log(+true)
-console.log(+"")
+// console.log(+true)
+// console.log(+"")
 
 
-let num1, num2,num3
-num1=num2=num3=2+2// not write that way it is write but not good sense for writting code in this way
+// let num1, num2,num3
+// num1=num2=num3=2+2// not write that way it is write but not good sense for writting code in this way
 
-let gameCounter = 100
-++gameCounter;
-console.log(gameCounter)
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter)
--- 01_basic/06_nums_and_math.js
+++ 01_basic/06_nums_and_math.js
@@ -24,10 +24,13 @@
 // console.log(Math.min(3,5,7,1,3));
 // console.log(Math.max(2,5,7,8,9,1,2));
 // values of random lies between 0 and 1
-console.log(Math.random());
-console.log((Math.random()*10) + 1);
-console.log(Math.floor(Math.random()*10) + 1);
-const min = 10
-const max = 20
 
-console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
+// const min = 10
+// const max = 20
+
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+console.log (Math.floor(Math.random()*6)+1);
